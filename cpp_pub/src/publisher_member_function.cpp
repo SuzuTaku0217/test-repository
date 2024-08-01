b729ddff76c085526578ce5bef105216da8bfe66
SuzuTaku0217 <hazutyoumin@yahoo.co.jp>
2024-08-01 22:28:09 +0900

    基本の形2

--- cpp_pub/src/publisher_member_function.cpp
+++ cpp_pub/src/publisher_member_function.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "test_interfaces/msg/num.hpp"
 
 using namespace std::chrono_literals;
 
@@ -17,7 +17,7 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("minimal_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<test_interfaces::msg::Num>("topic", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -25,13 +25,13 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      auto message = test_interfaces::msg::Num();
+      message.num = count_++;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.num);
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<test_interfaces::msg::Num>::SharedPtr publisher_;
     size_t count_;
 };
 
--- cpp_sub/src/subscriber_member_function.cpp
+++ cpp_sub/src/subscriber_member_function.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "test_interfaces/msg/num.hpp"
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
@@ -10,16 +10,16 @@ class MinimalSubscriber : public rclcpp::Node
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      subscription_ = this->create_subscription<test_interfaces::msg::Num>(
       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const std_msgs::msg::String & msg) const
+    void topic_callback(const test_interfaces::msg::Num & msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg.num);
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<test_interfaces::msg::Num>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
